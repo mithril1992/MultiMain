d0cec64b39702a12a65f6d32c8fcd5bc79996bd8
mithril1992 <nomber00@gmail.com>
2019-09-07 18:10:36 +0900

    プロジェクト本体の追加

---
+++ src/main/kotlin/io/github/mithril1992/main/Main.kt
@@ -0,0 +1,10 @@
+package io.github.mithril1992.main
+
+class Main {
+    companion object {
+        @JvmStatic
+        fun main(argv: Array<String>) {
+            println("main application")
+        }
+    }
+}
---
+++ src/main/kotlin/io/github/mithril1992/sub/Main.kt
@@ -0,0 +1,10 @@
+package io.github.mithril1992.sub
+
+class Main {
+    companion object {
+        @JvmStatic
+        fun main(argv: Array<String>) {
+            println("sub application")
+        }
+    }
+}
